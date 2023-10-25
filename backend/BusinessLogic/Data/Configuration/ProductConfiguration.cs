62b1b8672539372503d788cca6b8f8d991ddbe0b
Guillermo Calle <guillermocalle011@gmail.com>
2023-10-25 17:44:31 -0400

    Creating Base class, Brand and Category, Product entity configuration

---
+++ backend/BusinessLogic/Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(250);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(500);
+            builder.Property(p => p.Image).HasMaxLength(1000);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.HasOne(b => b.Brand).WithMany().HasForeignKey(p => p.BrandId);
+            builder.HasOne(c => c.Category).WithMany().HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
--- backend/Core/Entities/Product.cs
+++ backend/Core/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -7,19 +8,22 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseClass
     {
-        public int Id { get; set; }
-
         public string Name { get; set; }
 
         public string Description { get; set; } 
 
         public int Stock { get; set; }
 
-        public int Brand { get; set; }
+        public int BrandId { get; set; }
 
-        [Column(TypeName="decimal(18,4)")]
+        public Brand Brand { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
         public decimal Price { get; set; }
 
         public string Image { get; set; }
